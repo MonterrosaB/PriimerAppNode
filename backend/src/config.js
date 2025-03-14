ff5304536b08b67633b810c288e903c49bb3b74e
Rodrigo Monterrosa <20230197@ricaldone.edu.sv>
2025-03-14 12:07:38 -0600

    atlas db connection done

--- backend/src/config.js
+++ backend/src/config.js
@@ -4,7 +4,8 @@ dotenv.config()
 
 export const config = {
     db : {
-        URI : process.env.DB_URI || "mongodb://localhost:27017/NODE"
+        URI : process.env.DB_URI || "mongodb+srv://rmonterrosa:dObSxsR5BYz6LiV0@miprimercluster.87lyo.mongodb.net/nodejs"      
+
     },
     server : {
         PORT : process.env.PORT || 4000
